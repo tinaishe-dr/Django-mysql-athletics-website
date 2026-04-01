5f837087b5fbd6b74453621d5d4f4323825ababb
tinaishe-dr <61242468+tinaishe-dr@users.noreply.github.com>
2026-04-01 16:50:40 -0400

    templates and static directories config

--- mydatabaseproject/mydatabaseproject/settings.py
+++ mydatabaseproject/mydatabaseproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mydatabaseproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mydatabaseproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_project',
+        'USER': 'root',
+        'PASSWORD': 'Titi16071999',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -116,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']

2ea9a534947d27599cb9245e4953d81233c1d5c1
tinaishe-dr <61242468+tinaishe-dr@users.noreply.github.com>
2026-04-02 00:50:58 -0400

    added tables to display in admin page

--- mydatabaseproject/athleticsweb/admin.py
+++ mydatabaseproject/athleticsweb/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import t1_houseinfo, t1_houseinfoalt
 
 # Register your models here.
+admin.site.register(t1_houseinfo)
+admin.site.register(t1_houseinfoalt)
